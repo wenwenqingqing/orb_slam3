3b65da90006f44670e598b26934fd55eb78d20c3
yyqing <ypwenubuntu@gmail.com>
2020-09-23 20:19:35 +0800

    Add: InitialInertialGS gtsam factor

--- include/gtsam_optimizer.h
+++ include/gtsam_optimizer.h
@@ -127,7 +127,7 @@ namespace gtsam_wrapper
                            OptionalJacobian<9, 9> H2, OptionalJacobian<9, 3> H3) const;
     };
 
-    class GTSAM_EXPORT PreintegratedImuMeasurementsGS : public PreintegratedImuMeasurements
+    class GTSAM_EXPORT PreintegratedImuMeasurementsGS : public gtsam::PreintegratedImuMeasurements
     {
     public:
         NavState predict(const NavStateGS &state_i,
@@ -195,4 +195,61 @@ namespace gtsam_optimizer
                                  Eigen::Vector3d &ba, bool bMono, Eigen::MatrixXd &covInertial, bool bFixedVel, bool bGauss, float priorG, float priorA);
     };
 
+    class GTSAM_EXPORT InertialEgdeGS2 : public gtsam_wrapper::NoiseModelFactor7<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3,
+                                                                                 gtsam::Rot3, double, imuBias::ConstantBias>
+    {
+        typedef InertialEgdeGS2 This;
+        typedef gtsam_wrapper::NoiseModelFactor7<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3,
+                                                 gtsam::Rot3, double, imuBias::ConstantBias>
+            Base;
+
+        InertialEgdeGS2() {}
+
+    public:
+        InertialEgdeGS2(gtsam::Key posei_key, gtsam::Key veli_key, gtsam::Key posej_key, gtsam::Key velj_key,
+                        gtsam::Key rwg_key, gtsam::Key scale_key, gtsam::Key b_key, const gtsam::PreintegratedImuMeasurements &p)
+            : Base(noiseModel::Gaussian::Covariance(p.preintMeasCov()), posei_key, veli_key, posej_key, velj_key, rwg_key, scale_key, b_key), _PIM_(p), dt_(p.deltaTij()), gI_(p.params()->n_gravity)
+        {
+        }
+
+        static Eigen::Block<Vector9, 3, 1> dR(Vector9 &v)
+        {
+            return v.segment<3>(0);
+        }
+        static Eigen::Block<Vector9, 3, 1> dP(Vector9 &v)
+        {
+            return v.segment<3>(3);
+        }
+        static Eigen::Block<Vector9, 3, 1> dV(Vector9 &v)
+        {
+            return v.segment<3>(6);
+        }
+        static Eigen::Block<const Vector9, 3, 1> dR(const Vector9 &v)
+        {
+            return v.segment<3>(0);
+        }
+        static Eigen::Block<const Vector9, 3, 1> dP(const Vector9 &v)
+        {
+            return v.segment<3>(3);
+        }
+        static Eigen::Block<const Vector9, 3, 1> dV(const Vector9 &v)
+        {
+            return v.segment<3>(6);
+        }
+
+        gtsam::Vector evaluateError(gtsam::Pose3 &pose_i, gtsam::Vector3 &vel_i, gtsam::Pose3 &pose_j, gtsam::Vector3 &vel_j,
+                                    const Rot3 &rwg, const double &scale, imuBias::ConstantBias &bias_i,
+                                    boost::optional<Matrix &> H_pi,
+                                    boost::optional<Matrix &> H_vi,
+                                    boost::optional<Matrix &> H_pj,
+                                    boost::optional<Matrix &> H_vj,
+                                    boost::optional<Matrix &> H_rwg,
+                                    boost::optional<Matrix &> H_scale,
+                                    boost::optional<Matrix &> H_b) const;
+
+    private:
+        gtsam::PreintegratedImuMeasurements _PIM_;
+        double dt_;
+        gtsam::Vector3 gI_;
+    };
 }; // namespace gtsam_optimizer
