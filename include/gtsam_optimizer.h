ed9641370d8e1c54749a92c76d3b3a2644eeed5c
yyqing <ypwenubuntu@gmail.com>
2020-09-24 18:09:01 +0800

    Fix: remove unmeaningless gtsam

--- include/gtsam_optimizer.h
+++ include/gtsam_optimizer.h
@@ -113,48 +113,16 @@ namespace gtsam_wrapper
         }
     }; // \class NoiseModelFactor7
 
-    class GTSAM_EXPORT NavStateGS : public gtsam::NavState
-    {
-    public:
-        NavStateGS(const gtsam::Pose3 p, const gtsam::Vector v)
-            : NavState(p, v)
-        {
-        }
-
-        Vector9 correctPIM(const Vector9 &pim, double dt,
-                           const Vector3 &n_gravity, const boost::optional<Vector3> &omegaCoriolis,
-                           bool use2ndOrderCoriolis, OptionalJacobian<9, 9> H1,
-                           OptionalJacobian<9, 9> H2, OptionalJacobian<9, 3> H3) const;
-    };
-
-    class GTSAM_EXPORT PreintegratedImuMeasurementsGS : public gtsam::PreintegratedImuMeasurements
-    {
-    public:
-        NavState predict(const NavStateGS &state_i,
-                         const imuBias::ConstantBias &bias_i,
-                         const Vector3 &gdir, OptionalJacobian<9, 9> H1,
-                         OptionalJacobian<9, 6> H2, OptionalJacobian<9, 3> H3) const;
-
-        gtsam::Matrix9 PreintMeasCov() const;
-        gtsam::Vector9 computeError(const NavStateGS &state_i,
-                                    const NavState &state_j,
-                                    const imuBias::ConstantBias &bias_i,
-                                    const Vector3 &gdir,
-                                    OptionalJacobian<9, 9> H1,
-                                    OptionalJacobian<9, 9> H2,
-                                    OptionalJacobian<9, 6> H3,
-                                    OptionalJacobian<9, 3> H4) const;
-
-        gtsam::Vector9 computeErrorAndJacobians(const Pose3 &pose_i,
-                                                const Vector3 &vel_i, const Pose3 &pose_j, const Vector3 &vel_j,
-                                                const imuBias::ConstantBias &bias_i, const gtsam::Vector3 gdir, OptionalJacobian<9, 6> H1,
-                                                OptionalJacobian<9, 3> H2, OptionalJacobian<9, 6> H3,
-                                                OptionalJacobian<9, 3> H4, OptionalJacobian<9, 6> H5, OptionalJacobian<9, 3> H6) const;
-    };
 }; // namespace gtsam_wrapper
 
 namespace gtsam_optimizer
 {
+    class GtsamOptimizer
+    {
+        int InertialOptimization(ORB_SLAM3::Map *pMap, Eigen::Matrix3d &Rwg, double &scale, Eigen::Vector3d &bg,
+                                 Eigen::Vector3d &ba, bool bMono, Eigen::MatrixXd &covInertial, bool bFixedVel, bool bGauss, float priorG, float priorA);
+    };
+
     class GTSAM_EXPORT InertialEgdeGS : public gtsam_wrapper::NoiseModelFactor7<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3,
                                                                                 gtsam::Rot3, double, imuBias::ConstantBias>
     {
@@ -162,52 +130,12 @@ namespace gtsam_optimizer
         typedef gtsam_wrapper::NoiseModelFactor7<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3,
                                                  gtsam::Rot3, double, imuBias::ConstantBias>
             Base;
-        typedef gtsam_wrapper::PreintegratedImuMeasurementsGS PimGS;
 
         InertialEgdeGS() {}
 
     public:
         InertialEgdeGS(gtsam::Key posei_key, gtsam::Key veli_key, gtsam::Key posej_key, gtsam::Key velj_key,
-                       gtsam::Key rwg_key, gtsam::Key scale_key, gtsam::Key b_key, const PimGS &p)
-            : Base(noiseModel::Gaussian::Covariance(p.PreintMeasCov()), posei_key, veli_key, posej_key, velj_key, rwg_key, scale_key, b_key), _PIM_(p), dt_(p.deltaTij())
-        {
-        }
-
-        gtsam::Vector evaluateError(gtsam::Pose3 &pose_i, gtsam::Vector3 &vel_i, gtsam::Pose3 &pose_j, gtsam::Vector3 &vel_j,
-                                    const Rot3 &rwg, const double &scale, imuBias::ConstantBias &bias_i,
-                                    boost::optional<Matrix &> H_pi,
-                                    boost::optional<Matrix &> H_vi,
-                                    boost::optional<Matrix &> H_pj,
-                                    boost::optional<Matrix &> H_vj,
-                                    boost::optional<Matrix &> H_rwg,
-                                    boost::optional<Matrix &> H_scale,
-                                    boost::optional<Matrix &> H_b) const;
-
-    private:
-        PimGS _PIM_;
-        double dt_;
-        gtsam::Vector3 gI_;
-    };
-
-    class GtsamOptimizer
-    {
-        int InertialOptimization(ORB_SLAM3::Map *pMap, Eigen::Matrix3d &Rwg, double &scale, Eigen::Vector3d &bg,
-                                 Eigen::Vector3d &ba, bool bMono, Eigen::MatrixXd &covInertial, bool bFixedVel, bool bGauss, float priorG, float priorA);
-    };
-
-    class GTSAM_EXPORT InertialEgdeGS2 : public gtsam_wrapper::NoiseModelFactor7<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3,
-                                                                                 gtsam::Rot3, double, imuBias::ConstantBias>
-    {
-        typedef InertialEgdeGS2 This;
-        typedef gtsam_wrapper::NoiseModelFactor7<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3,
-                                                 gtsam::Rot3, double, imuBias::ConstantBias>
-            Base;
-
-        InertialEgdeGS2() {}
-
-    public:
-        InertialEgdeGS2(gtsam::Key posei_key, gtsam::Key veli_key, gtsam::Key posej_key, gtsam::Key velj_key,
-                        gtsam::Key rwg_key, gtsam::Key scale_key, gtsam::Key b_key, const gtsam::PreintegratedImuMeasurements &p)
+                       gtsam::Key rwg_key, gtsam::Key scale_key, gtsam::Key b_key, const gtsam::PreintegratedImuMeasurements &p)
             : Base(noiseModel::Gaussian::Covariance(p.preintMeasCov()), posei_key, veli_key, posej_key, velj_key, rwg_key, scale_key, b_key), _PIM_(p), dt_(p.deltaTij()), gI_(p.params()->n_gravity)
         {
         }
